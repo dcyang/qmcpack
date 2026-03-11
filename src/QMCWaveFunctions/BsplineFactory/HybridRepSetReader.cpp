dcyang/qmcpack

commit 20962cea50c076119c620cf70ee4d5f7737f9871

Move spline table gatherv and bcast to SplineUtils.

src/QMCWaveFunctions/BsplineFactory/HybridRepSetReader.cpp
@@ -155,7 +155,7 @@ std::unique_ptr<SPOSet> HybridRepSetReader<SA>::create_spline_set(const std::str
     hdf_archive h5f(myComm);
     const auto splinefile = getSplineDumpFileName(bandgroup);
     h5f.open(splinefile, H5F_ACC_RDONLY);
-    foundspline = SplineUtils<DataType>::read(bspline->getMultiSpline3D(), h5f) && bspline->read_atomic_splines(h5f);
+    foundspline = SplineUtils<DataType>::read(*bspline->SplineInst, h5f) && bspline->read_atomic_splines(h5f);
     if (foundspline)
       app_log() << "  Successfully restored 3D B-spline coefficients from " << splinefile << ". The reading time is "
                 << now.elapsed() << " sec." << std::endl;
@@ -176,7 +176,7 @@ std::unique_ptr<SPOSet> HybridRepSetReader<SA>::create_spline_set(const std::str
       h5f.write(classname, "class_name");
       int sizeD = sizeof(DataType);
       h5f.write(sizeD, "sizeof");
-      SplineUtils<DataType>::write(bspline->getMultiSpline3D(), h5f);
+      SplineUtils<DataType>::write(*bspline->SplineInst, h5f);
       bspline->write_atomic_splines(h5f);
       h5f.close();
       app_log() << "  Stored spline coefficients in " << splinefile << " for potential reuse. The writing time is "
@@ -186,7 +186,7 @@ std::unique_ptr<SPOSet> HybridRepSetReader<SA>::create_spline_set(const std::str
 
   {
     Timer now;
-    bspline->bcast_tables(myComm);
+    SplineUtils<DataType>::bcast(*bspline->SplineInst, *myComm);
     app_log() << "  Time to bcast the table = " << now.elapsed() << std::endl;
   }
   return bspline;
@@ -653,7 +653,20 @@ void HybridRepSetReader<SA>::initialize_hybrid_pio_gather(const int spin,
   if (band_group_comm.isGroupLeader())
   {
     Timer now;
-    bspline.gather_tables(band_group_comm.getGroupLeaderComm());
+    if (bspline.isComplex())
+    {
+      std::vector<int> offset(band_groups.size());
+      for (int i = 0; i < offset.size(); i++)
+        offset[i] = band_groups[i] * 2;
+      SplineUtils<DataType>::gatherv(*bspline.SplineInst, offset, *band_group_comm.getGroupLeaderComm());
+      bspline.gather_atomic_tables(band_group_comm.getGroupLeaderComm(), offset);
+    }
+    else
+    {
+      SplineUtils<DataType>::gatherv(*bspline.SplineInst, band_groups, *band_group_comm.getGroupLeaderComm());
+      bspline.gather_atomic_tables(band_group_comm.getGroupLeaderComm(), band_groups);
+    }
+
     app_log() << "  Time to gather the table = " << now.elapsed() << std::endl;
   }
 }

src/QMCWaveFunctions/BsplineFactory/BsplineSet.h
@@ -22,8 +22,6 @@
 #define QMCPLUSPLUS_BSPLINESET_H
 
 #include "QMCWaveFunctions/SPOSet.h"
-#include "spline/einspline_engine.hpp"
-#include "spline/einspline_util.hpp"
 
 namespace qmcplusplus
 {
@@ -50,8 +48,6 @@ protected:
   std::vector<SPOSet::PosType> kPoints;
   ///remap splines to orbitals
   aligned_vector<int> BandIndexMap;
-  ///band offsets used for communication
-  std::vector<int> offset;
 
 public:
   BsplineSet(const std::string& my_name) : SPOSet(my_name), first_spo(0), last_spo(0) {}

src/QMCWaveFunctions/BsplineFactory/HybridRepCenterOrbitals.h
@@ -95,10 +95,10 @@ public:
     create_spline();
   }
 
-  void bcast_tables(Communicate* comm) { chunked_bcast(comm, SplineInst->getSplinePtr()); }
+  void bcast_tables(Communicate& comm) { SplineUtils<ST>::bcast(*SplineInst, comm); }
 
-  void gather_tables(Communicate* comm, std::vector<int>& offset)
-  { gatherv(comm, SplineInst->getSplinePtr(), Npad, offset); }
+  void gather_tables(Communicate& comm, const std::vector<int>& offset)
+  { SplineUtils<ST>::gatherv(*SplineInst, Npad, offset, comm); }
 
   template<typename PT, typename VT>
   inline void set_info(const PT& R,
@@ -571,18 +571,18 @@ public:
               << "for the atomic radial splines in hybrid orbital representation" << std::endl;
   }
 
-  void bcast_tables(Communicate* comm)
+  void bcast_atomic_tables(Communicate* comm)
   {
     for (int ic = 0; ic < AtomicCenters.size(); ic++)
-      AtomicCenters[ic].bcast_tables(comm);
+      AtomicCenters[ic].bcast_tables(*comm);
   }
 
-  void gather_atomic_tables(Communicate* comm, std::vector<int>& offset)
+  void gather_atomic_tables(Communicate* comm, const std::vector<int>& offset)
   {
     if (comm->size() == 1)
       return;
     for (int ic = 0; ic < AtomicCenters.size(); ic++)
-      AtomicCenters[ic].gather_tables(comm, offset);
+      AtomicCenters[ic].gather_tables(*comm, offset);
   }
 
   inline void flush_zero()

src/QMCWaveFunctions/BsplineFactory/HybridRepCplx.h
@@ -67,8 +67,6 @@ private:
 public:
   HybridRepCplx(const std::string& my_name) : SPLINEBASE(my_name) {}
 
-  auto& getMultiSpline3D() {return *SPLINEBASE::SplineInst; }
-
   bool isRotationSupported() const override { return SPLINEBASE::isRotationSupported(); }
   void storeParamsBeforeRotation() override
   {
@@ -86,18 +84,6 @@ public:
   bool isOMPoffload() const final { return false; }
 
   std::unique_ptr<SPOSet> makeClone() const override { return std::make_unique<HybridRepCplx>(*this); }
-
-  void bcast_tables(Communicate* comm)
-  {
-    SPLINEBASE::bcast_tables(comm);
-    HYBRIDBASE::bcast_tables(comm);
-  }
-
-  void gather_tables(Communicate* comm)
-  {
-    SPLINEBASE::gather_tables(comm);
-    HYBRIDBASE::gather_atomic_tables(comm, SPLINEBASE::offset);
-  }
 
   void evaluateValue(const ParticleSet& P, const int iat, ValueVector& psi) override
   {
@@ -162,9 +148,7 @@ public:
                             const RefVector<ValueVector>& psi_list,
                             const std::vector<const ValueType*>& invRow_ptr_list,
                             std::vector<std::vector<ValueType>>& ratios_list) const final
-  {
-    BsplineSet::mw_evaluateDetRatios(spo_list, vp_list, psi_list, invRow_ptr_list, ratios_list);
-  }
+  { BsplineSet::mw_evaluateDetRatios(spo_list, vp_list, psi_list, invRow_ptr_list, ratios_list); }
 
   void evaluateVGL(const ParticleSet& P, const int iat, ValueVector& psi, GradVector& dpsi, ValueVector& d2psi) override
   {
@@ -190,9 +174,7 @@ public:
                       const RefVector<ValueVector>& psi_v_list,
                       const RefVector<GradVector>& dpsi_v_list,
                       const RefVector<ValueVector>& d2psi_v_list) const final
-  {
-    BsplineSet::mw_evaluateVGL(sa_list, P_list, iat, psi_v_list, dpsi_v_list, d2psi_v_list);
-  }
+  { BsplineSet::mw_evaluateVGL(sa_list, P_list, iat, psi_v_list, dpsi_v_list, d2psi_v_list); }
 
   void mw_evaluateVGLandDetRatioGrads(const RefVectorWithLeader<SPOSet>& spo_list,
                                       const RefVectorWithLeader<ParticleSet>& P_list,
@@ -201,9 +183,7 @@ public:
                                       OffloadMWVGLArray& phi_vgl_v,
                                       std::vector<ValueType>& ratios,
                                       std::vector<GradType>& grads) const final
-  {
-    BsplineSet::mw_evaluateVGLandDetRatioGrads(spo_list, P_list, iat, invRow_ptr_list, phi_vgl_v, ratios, grads);
-  }
+  { BsplineSet::mw_evaluateVGLandDetRatioGrads(spo_list, P_list, iat, invRow_ptr_list, phi_vgl_v, ratios, grads); }
 
   void evaluateVGH(const ParticleSet& P,
                    const int iat,
@@ -225,9 +205,7 @@ public:
                      GradVector& dpsi,
                      HessVector& grad_grad_psi,
                      GGGVector& grad_grad_grad_psi) override
-  {
-    APP_ABORT("HybridRepCplx::evaluate_vghgh not implemented!");
-  }
+  { APP_ABORT("HybridRepCplx::evaluate_vghgh not implemented!"); }
 
   void evaluate_notranspose(const ParticleSet& P,
                             int first,

src/QMCWaveFunctions/BsplineFactory/HybridRepReal.h
@@ -69,8 +69,6 @@ private:
 public:
   HybridRepReal(const std::string& my_name) : SPLINEBASE(my_name) {}
 
-  auto& getMultiSpline3D() {return *SPLINEBASE::SplineInst; }
-
   bool isRotationSupported() const override { return SPLINEBASE::isRotationSupported(); }
   void storeParamsBeforeRotation() override
   {
@@ -88,18 +86,6 @@ public:
   bool isOMPoffload() const final { return false; }
 
   std::unique_ptr<SPOSet> makeClone() const override { return std::make_unique<HybridRepReal>(*this); }
-
-  void bcast_tables(Communicate* comm)
-  {
-    SPLINEBASE::bcast_tables(comm);
-    HYBRIDBASE::bcast_tables(comm);
-  }
-
-  void gather_tables(Communicate* comm)
-  {
-    SPLINEBASE::gather_tables(comm);
-    HYBRIDBASE::gather_atomic_tables(comm, SPLINEBASE::offset);
-  }
 
   void evaluateValue(const ParticleSet& P, const int iat, ValueVector& psi) override
   {
@@ -166,9 +152,7 @@ public:
                             const RefVector<ValueVector>& psi_list,
                             const std::vector<const ValueType*>& invRow_ptr_list,
                             std::vector<std::vector<ValueType>>& ratios_list) const final
-  {
-    BsplineSet::mw_evaluateDetRatios(spo_list, vp_list, psi_list, invRow_ptr_list, ratios_list);
-  }
+  { BsplineSet::mw_evaluateDetRatios(spo_list, vp_list, psi_list, invRow_ptr_list, ratios_list); }
 
   void evaluateVGL(const ParticleSet& P, const int iat, ValueVector& psi, GradVector& dpsi, ValueVector& d2psi) override
   {
@@ -196,9 +180,7 @@ public:
                       const RefVector<ValueVector>& psi_v_list,
                       const RefVector<GradVector>& dpsi_v_list,
                       const RefVector<ValueVector>& d2psi_v_list) const final
-  {
-    BsplineSet::mw_evaluateVGL(sa_list, P_list, iat, psi_v_list, dpsi_v_list, d2psi_v_list);
-  }
+  { BsplineSet::mw_evaluateVGL(sa_list, P_list, iat, psi_v_list, dpsi_v_list, d2psi_v_list); }
 
   void mw_evaluateVGLandDetRatioGrads(const RefVectorWithLeader<SPOSet>& spo_list,
                                       const RefVectorWithLeader<ParticleSet>& P_list,
@@ -207,9 +189,7 @@ public:
                                       OffloadMWVGLArray& phi_vgl_v,
                                       std::vector<ValueType>& ratios,
                                       std::vector<GradType>& grads) const final
-  {
-    BsplineSet::mw_evaluateVGLandDetRatioGrads(spo_list, P_list, iat, invRow_ptr_list, phi_vgl_v, ratios, grads);
-  }
+  { BsplineSet::mw_evaluateVGLandDetRatioGrads(spo_list, P_list, iat, invRow_ptr_list, phi_vgl_v, ratios, grads); }
 
   void evaluateVGH(const ParticleSet& P,
                    const int iat,
@@ -232,9 +212,7 @@ public:
                      GradVector& dpsi,
                      HessVector& grad_grad_psi,
                      GGGVector& grad_grad_grad_psi) override
-  {
-    APP_ABORT("HybridRepCplx::evaluateVGHGH not implemented!");
-  }
+  { APP_ABORT("HybridRepCplx::evaluateVGHGH not implemented!"); }
 
   void evaluate_notranspose(const ParticleSet& P,
                             int first,

src/QMCWaveFunctions/BsplineFactory/SplineC2C.h
@@ -119,21 +119,6 @@ public:
     mygH.resize(npad);
   }
 
-  void bcast_tables(Communicate* comm) { chunked_bcast(comm, SplineInst->getSplinePtr()); }
-
-  void gather_tables(Communicate* comm)
-  {
-    if (comm->size() == 1)
-      return;
-    const int Nbands      = kPoints.size();
-    const int Nbandgroups = comm->size();
-    offset.resize(Nbandgroups + 1, 0);
-    FairDivideLow(Nbands, Nbandgroups, offset);
-    for (size_t ib = 0; ib < offset.size(); ib++)
-      offset[ib] *= 2;
-    gatherv(comm, SplineInst->getSplinePtr(), SplineInst->getSplinePtr()->z_stride, offset);
-  }
-
   template<typename BCT>
   void create_spline(const Ugrid xyz_g[3], const BCT& xyz_bc)
   {

src/QMCWaveFunctions/BsplineFactory/SplineC2COMPTarget.h
@@ -126,9 +126,7 @@ public:
   virtual bool isOMPoffload() const override { return true; }
 
   void createResource(ResourceCollection& collection) const override
-  {
-    auto resource_index = collection.addResource(std::make_unique<SplineOMPTargetMultiWalkerMem<ST, ComplexT>>());
-  }
+  { auto resource_index = collection.addResource(std::make_unique<SplineOMPTargetMultiWalkerMem<ST, ComplexT>>()); }
 
   void acquireResource(ResourceCollection& collection, const RefVectorWithLeader<SPOSet>& spo_list) const override
   {
@@ -162,22 +160,6 @@ public:
     myL.resize(npad);
     myH.resize(npad);
     mygH.resize(npad);
-  }
-
-  void bcast_tables(Communicate* comm) { chunked_bcast(comm, SplineInst->getSplinePtr()); }
-
-  void gather_tables(Communicate* comm)
-  {
-    if (comm->size() == 1)
-      return;
-    const int Nbands      = kPoints.size();
-    const int Nbandgroups = comm->size();
-    offset.resize(Nbandgroups + 1, 0);
-    FairDivideLow(Nbands, Nbandgroups, offset);
-
-    for (size_t ib = 0; ib < offset.size(); ib++)
-      offset[ib] *= 2;
-    gatherv(comm, SplineInst->getSplinePtr(), SplineInst->getSplinePtr()->z_stride, offset);
   }
 
   template<typename BCT>

src/QMCWaveFunctions/BsplineFactory/SplineC2R.h
@@ -104,22 +104,6 @@ public:
     mygH.resize(npad);
   }
 
-  void bcast_tables(Communicate* comm) { chunked_bcast(comm, SplineInst->getSplinePtr()); }
-
-  void gather_tables(Communicate* comm)
-  {
-    if (comm->size() == 1)
-      return;
-    const int Nbands      = kPoints.size();
-    const int Nbandgroups = comm->size();
-    offset.resize(Nbandgroups + 1, 0);
-    FairDivideLow(Nbands, Nbandgroups, offset);
-
-    for (size_t ib = 0; ib < offset.size(); ib++)
-      offset[ib] = offset[ib] * 2;
-    gatherv(comm, SplineInst->getSplinePtr(), SplineInst->getSplinePtr()->z_stride, offset);
-  }
-
   template<typename BCT>
   void create_spline(const Ugrid xyz_g[3], const BCT& xyz_bc)
   {

commit c19c6e7d85f7fcce5cd137e97086fbc7b53a9db0

Add bcast atomic spline table. fixing a bug introduced in 20962ce

src/QMCWaveFunctions/BsplineFactory/HybridRepSetReader.cpp
@@ -156,7 +156,7 @@ std::unique_ptr<SPOSet> HybridRepSetReader<SA>::create_spline_set(const std::str
     hdf_archive h5f(myComm);
     const auto splinefile = getSplineDumpFileName(bandgroup);
     h5f.open(splinefile, H5F_ACC_RDONLY);
-    foundspline = SplineUtils<DataType>::read(*bspline->SplineInst, h5f) && bspline->read_atomic_splines(h5f);
+    foundspline = SplineUtils<DataType>::read(*bspline->SplineInst, h5f) && hybrid_center_orbs.read_atomic_splines(h5f);
     if (foundspline)
       app_log() << "  Successfully restored 3D B-spline coefficients from " << splinefile << ". The reading time is "
                 << now.elapsed() << " sec." << std::endl;
@@ -178,7 +178,7 @@ std::unique_ptr<SPOSet> HybridRepSetReader<SA>::create_spline_set(const std::str
       int sizeD = sizeof(DataType);
       h5f.write(sizeD, "sizeof");
       SplineUtils<DataType>::write(*bspline->SplineInst, h5f);
-      bspline->write_atomic_splines(h5f);
+      hybrid_center_orbs.write_atomic_splines(h5f);
       h5f.close();
       app_log() << "  Stored spline coefficients in " << splinefile << " for potential reuse. The writing time is "
                 << now.elapsed() << " sec." << std::endl;
@@ -188,6 +188,7 @@ std::unique_ptr<SPOSet> HybridRepSetReader<SA>::create_spline_set(const std::str
   {
     Timer now;
     SplineUtils<DataType>::bcast(*bspline->SplineInst, *myComm);
+    hybrid_center_orbs.bcast_atomic_tables(myComm);
     app_log() << "  Time to bcast the table = " << now.elapsed() << std::endl;
   }
   return bspline;